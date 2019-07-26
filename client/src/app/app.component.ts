81e8f83b00df1e66a6279a77db10cd7db168a0cd
Ahmad Hassan <ahmad.pgu1@hotmail.com>
2019-07-26 23:55:17 +0500

    basic table layout with add functionality
    ++ bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { notStrictEqual } from 'assert';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss', "../../node_modules/bootstrap/dist/css/bootstrap.min.css"]
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Notes Manager';
+  addNote = function(e){
+    if (e.keyCode == 13){
+      this.notes.push({id:notStrictEqual.length,comment:e.target.value});
+      e.target.value="";
+
+    }
+  }
+  notes = [
+    {
+      id: "1",
+      comment: "description for sub1"
+    },
+    {
+      id: "2",
+      comment: "description for sub2"
+    },
+    {
+      id: "3",
+      comment: "description for sub3"
+    }
+  ]
 }
